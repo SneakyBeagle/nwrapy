be5362b1c052cc989a2fabf8bcba26c4a3346a71
jochemste <jochemstevense@hotmail.com>
2022-01-11 08:41:15 +0100

    created setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='nwrapy',
+      version='0.1.0',
+      description='Nmap wrapper',
+      author='Jochem Stevense',
+      author_email='',
+      url='https://github.com/SneakyBeagle/nwrapy',
+      packages=[],
+      license='GPLv3',
+      install_requires=[],
+      classifiers=[],
+      entry_points={
+          }
+      )
